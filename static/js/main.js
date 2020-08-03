c44d31e28467260366dd7b6d5b7495acc3ab2f69
uttam580 <choudharyuttam94@gmail.com>
2020-08-04 04:08:03 +0530

    js for interact with UI

---
+++ static/js/main.js
@@ -0,0 +1,22 @@
+function getBotResponse() {
+    var rawText = $("#textInput").val();
+    var userHtml = '<p class="userText"><span>' + rawText + '</span></p>';
+    $("#textInput").val("");
+    $("#chatbox").append(userHtml);
+    document.getElementById('userInput').scrollIntoView({block: 'start', behavior: 'smooth'});
+    $.get("/get", { msg: rawText }).done(function(data) {
+      var botHtml = '<p class="botText"><span>' + data + '</span></p>';
+      $("#chatbox").append(botHtml);
+      document.getElementById('userInput').scrollIntoView({block: 'start', behavior: 'smooth'});
+    });
+  }
+  $("#textInput").keypress(function(e) {
+      if ((e.which == 13) && document.getElementById("textInput").value != "" ){
+          getBotResponse();
+      }
+  });
+  $("#buttonInput").click(function() {
+      if (document.getElementById("textInput").value != "") {
+          getBotResponse();
+      }
+  })
